561754fff65620fee92efa84d59fcf8012850bff
Max Mishyn <maxim.mn@gmail.com>
2025-05-28 00:40:49 +0300

    Move records count header to the middleware

---
+++ app/Http/Middleware/AddCarTotalCountHeader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Car;
+use Closure;
+use Illuminate\Http\Request;
+
+class AddCarTotalCountHeader
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($request->is('api/cars') && $request->isMethod('GET')) {
+            // ToDo: Its implemented as a quick workaround as long as there are no filtering in api
+            $totalCount = Car::count();
+            $response->headers->set('X-Total-Count', $totalCount);
+        }
+
+        return $response;
+    }
+}
