8a030c0b0b097f47eb46273702d064681a97cb05
Max Mishyn <maxim.mn@gmail.com>
2025-05-27 23:00:02 +0300

    API resources definitions. Documentation

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -3,12 +3,67 @@
 namespace App\Models;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\OpenApi\Model\Operation;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Symfony\Component\Serializer\Attribute\Groups;
 
-#[ApiResource()]
+#[ApiResource(
+    operations: [
+        new GetCollection(
+            openapi: new Operation(
+                summary: 'List of all cars',
+                description: 'Retrieves a collection of cars available.',
+            ),
+        ),
+        new Get(
+            openapi: new Operation(
+                summary: 'Get a specific car',
+                description: 'Retrieves details of a specific car by its ID.',
+            ),
+        ),
+        new Post(
+            openapi: new Operation(
+                summary: 'Create a specific car',
+                description: 'Creates a new car entry in the database.',
+            ),
+        ),
+        new Put(
+            openapi: new Operation(
+                summary: 'Update a specific car',
+                description: 'Updates the details of an existing car by its ID.',
+            )
+        ),
+        new Patch(
+            openapi: new Operation(
+                summary: 'Partially update a specific car',
+                description: 'Partially updates the details of an existing car by its ID.',
+            )
+        ),
+        new Delete(
+            openapi: new Operation(
+                summary: 'Delete a specific car',
+                description: 'Deletes a specific car entry from the database by its ID.',
+            )
+        ),
+
+    ],
+    normalizationContext: ['groups' => ['car']],
+    denormalizationContext: ['groups' => ['car']],
+    rules: [
+        'make' => 'required',
+        'model' => 'required',
+        'year' => 'required|integer|min:2000|max:2025',
+        'price' => 'required|integer|min:100',
+    ],
+)]
 class Car extends Model
 {
     /** @use HasFactory<\Database\Factories\CarFactory> */
